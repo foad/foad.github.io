2e255b499616a51743aa807c18737f42beb08a4c
DanFoad <daniel.foad@sky.uk>
2018-12-26 18:37:27 +0000

    Fixed proptype warning

--- src/containers/home/home-container.js
+++ src/containers/home/home-container.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import PropTypes from 'prop-types';
 
 import { init } from '../../services/home-service';
 
