ab02a740ac34c52a17f1eb8e7947127faa2b0f2f
DanFoad <daniel.foad@sky.uk>
2018-12-28 21:50:16 +0000

    Got pipes rendering properly

--- src/components/home/home-header-pipeline.js
+++ src/components/home/home-header-pipeline.js
@@ -9,23 +9,26 @@ export class HomeHeaderPipeline extends React.Component {
 
     this.state = {
       config: {
-        pipeCount: 8,
-        minLength: 100,
-        maxLength: 400,
+        pipeCount: 15,
+        maxDelay: 200,
+        minLength: 300,
+        maxLength: 1000,
         minRadius: 2,
         maxRadius: 6,
-        minSpeed: 1,
-        maxSpeed: 1,
+        minSpeed: 0.75,
+        maxSpeed: 0.3,
         minLifetime: 0,
-        maxLifeTime: 0,
+        maxLifeTime: 20,
         minX: 0,
         maxX: 0,
         minY: 0,
         maxY: 0,
-        maxTurns: 5,
+        minTurns: 2,
+        maxTurns: 8,
+        minOpacity: 0.25,
+        maxOpacity: 0.5,
       },
       pipes: [],
-      tick: 0,
     };
 
     this.canvas = React.createRef();
@@ -37,7 +40,8 @@ export class HomeHeaderPipeline extends React.Component {
         this.generatePipes();
       })
       .then(() => {
-        this.draw();
+        const ctx = this.canvas.current.getContext('2d');
+        this.draw(0, ctx);
       })
       .catch(() => {});
   }
@@ -45,12 +49,16 @@ export class HomeHeaderPipeline extends React.Component {
   setup() {
     return new Promise(resolve => {
       const headerLeft = document.querySelector('.home-header__name').getBoundingClientRect().left - 128;
+      const rect = document.querySelector('.home-header').getBoundingClientRect();
+      this.canvas.current.width = rect.width;
+      this.canvas.current.height = rect.height;
       this.setState(
         prevState => ({
           config: {
             ...prevState.config,
             maxX: headerLeft,
-            maxY: this.canvas.current.getBoundingClientRect().height,
+            minY: 0 + this.canvas.current.height / 4,
+            maxY: this.canvas.current.height - this.canvas.current.height / 4,
           },
         }),
         resolve
@@ -62,40 +70,50 @@ export class HomeHeaderPipeline extends React.Component {
     return new Promise(resolve => {
       const pipes = [];
 
-      for (let i = 0; i < this.state.pipeCount; i++) {
-        pipes.push(this.generatePipe(i));
+      for (let i = 0; i < this.state.config.pipeCount; i++) {
+        pipes.push(this.generatePipe(i, 0));
       }
 
       this.setState({ pipes }, resolve);
     });
   }
 
-  generatePipe(i) {
+  generatePipe(i, tick) {
     const config = this.state.config;
     const turns = [];
-    const turnCount = p.randIn(0, config.maxTurns);
-    const fullLength = p.randIn(config.minLength, config.maxLength);
+    const turnCount = p.randIn(config.minTurns, config.maxTurns);
+    const fullLength = p.floor(p.randIn(config.minLength, config.maxLength));
 
     for (let turn = 0; turn < turnCount; turn++) {
-      turns.push([p.randIn(0, fullLength), p.round(p.rand(1)) ? -1 : 1]);
+      turns.push([p.floor(p.randIn(0, fullLength)), p.round(p.rand(1)) ? -1 : 1]);
     }
 
     const pipe = {
       pipeId: i,
       fullLength,
-      pipeRadius: p.randIn(config.minRadius, config.maxRadius),
+      startingTick: p.floor(p.randIn(tick, tick + config.maxDelay)),
+      pipeRadius: p.round(p.randIn(config.minRadius, config.maxRadius)),
       speed: p.randIn(config.minSpeed, config.maxSpeed),
       lifetime: p.randIn(config.minLifetime, config.maxLifeTime),
-      startPos: [p.randIn(config.minX, config.maxX), p.randIn(config.minY, config.maxY)],
-      startDirection: p.randIn(0, 8) * (p.PI / 4),
+      startPos: [p.floor(p.randIn(config.minX, config.maxX)), p.floor(p.randIn(config.minY, config.maxY))],
+      startDirection: p.round(p.randIn(0, 8)) * (p.PI / 4),
+      baseOpacity: p.randIn(config.minOpacity, config.maxOpacity),
       turns,
     };
 
     return pipe;
   }
 
-  draw() {
-    const ctx = this.canvas.current.getContext('2d');
+  draw(tick, ctx) {
+    this.clearCanvas(ctx);
+    this.renderPipes(tick, ctx);
+
+    window.requestAnimationFrame(() => {
+      this.draw(tick + 1, ctx);
+    });
+  }
+
+  clearCanvas(ctx) {
     ctx.save();
     ctx.fillStyle = '#1e282e';
     ctx.fillRect(
@@ -105,7 +123,60 @@ export class HomeHeaderPipeline extends React.Component {
       this.canvas.current.getBoundingClientRect().height
     );
     ctx.restore();
-    return p.TAU;
+  }
+
+  renderPipes(tick, ctx) {
+    const pipeCount = this.state.config.pipeCount;
+    const pipes = this.state.pipes;
+
+    for (let i = 0; i < pipeCount; i++) {
+      this.renderPipe(pipes[i], tick, ctx);
+    }
+  }
+
+  renderPipe(pipe, tick, ctx) {
+    if (tick < pipe.startingTick) return;
+
+    const fullyDrawn = pipe.startingTick + pipe.fullLength * pipe.speed;
+    const startFade = fullyDrawn + pipe.lifetime;
+    const finalTick = startFade + pipe.fullLength * pipe.speed;
+
+    if (tick >= finalTick) {
+      this.setState(prevState => {
+        prevState.pipes[pipe.pipeId] = this.generatePipe(pipe.pipeId, tick);
+        return { pipes: prevState.pipes };
+      });
+    }
+
+    let [x, y, direction] = [...pipe.startPos, pipe.startDirection];
+
+    for (let i = 0; i < pipe.fullLength; i++) {
+      [x, y, direction] = this.renderArc(pipe, tick, i, ctx, x, y, direction, startFade, finalTick);
+    }
+  }
+
+  renderArc(pipe, tick, i, ctx, x, y, direction, startFade) {
+    const startingOpacity = p.fadeInOut(i * pipe.speed, pipe.fullLength) * pipe.baseOpacity;
+
+    let opacity = startingOpacity;
+    if (tick - pipe.startingTick < i * pipe.speed) opacity = 0;
+    if (tick >= startFade) {
+      if (tick - startFade > i * pipe.speed) opacity = 0;
+      else opacity = p.fadeInOut(i * pipe.speed - tick + startFade, pipe.fullLength) * pipe.baseOpacity;
+    }
+
+    ctx.strokeStyle = `rgba(255, 255, 255, ${opacity})`;
+    ctx.beginPath();
+    ctx.arc(x, y, pipe.pipeRadius, 0, p.TAU);
+    ctx.stroke();
+    ctx.closePath();
+
+    let newDirection = direction;
+    pipe.turns.forEach(t => {
+      if (i === t[0]) newDirection = direction + (p.PI / 4) * t[1];
+    });
+
+    return [x + p.cos(newDirection), y + p.sin(newDirection), newDirection];
   }
 
   render() {
