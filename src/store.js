c036d53e847eded15d3e5c24d4f93637a3303bb8
DanFoad <daniel.foad@sky.uk>
2018-12-30 18:50:53 +0000

    Disable redux devtools in production

--- src/store.js
+++ src/store.js
@@ -4,10 +4,16 @@ import thunk from 'redux-thunk';
 import { reducers } from './reducers/reducers';
 
 const reducer = combineReducers(reducers);
+let devtools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+  devtools = a => a;
+}
+
 export const store = createStore(
   reducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devtools
   )
 );
