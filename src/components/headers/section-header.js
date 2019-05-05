a6e003e14eb8ec6c804bc8ee5af22b9cacb68515
Dan Foad <danielfoad@gmail.com>
2019-05-05 19:00:10 +0100

    New header styling

--- src/components/headers/section-header.js
+++ src/components/headers/section-header.js
@@ -5,10 +5,7 @@ export class SectionHeader extends React.Component {
   render() {
     return (
       <div className="section-header">
-        <h2>
-          {this.props.children}
-          <div className="highlight" />
-        </h2>
+        <h2>{this.props.children}</h2>
       </div>
     );
   }
