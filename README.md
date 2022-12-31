4e84d29fc00f2415b34a880f4e19fdfd2ec9e732
Daniel Foad <danielfoad@gmail.com>
2022-12-31 16:54:31 +0000

    clean slate

--- README.md
+++
@@ -1,6 +0,0 @@
-# foad.dev
-
-![Test](https://github.com/foad/foad.dev/workflows/Test/badge.svg)
-![Build & Deploy](https://github.com/foad/foad.dev/workflows/Build%20&%20Deploy/badge.svg)
-
-Repo for my website https://foad.dev
--- src/modules/app/app-selector.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-export const appSelector = createSelector([state => state.app], app => {
-  return {
-    navExpanded: app.navExpanded,
-    transparentNav: app.transparentNav,
-  };
-});
