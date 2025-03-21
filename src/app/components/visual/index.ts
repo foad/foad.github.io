2afda4b52ac250787a9a09b7d6df5bd7203ec29e
Dan Foad <danielfoad@gmail.com>
2025-03-21 20:20:05 +0000

    contour visual item

---
+++ src/app/components/visual/index.ts
@@ -0,0 +1,3 @@
+import { Contour } from "./contour";
+
+export { Contour };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Title } from "./components/title";
 import { Panel } from "./components/panel";
+import { Contour } from "./components/visual";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -7,12 +8,15 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Dan Foad</h1>
-        <Panel>
-          <Title variant="main">Power Units</Title>
-          <Title variant="secondary" subtitle="active">
-            Unit 01
-          </Title>
-        </Panel>
+        <div className={styles.grid}>
+          <Panel>
+            <Title variant="main">Power Units</Title>
+            <Contour />
+            <Title variant="secondary" subtitle="active">
+              Unit 01
+            </Title>
+          </Panel>
+        </div>
       </main>
     </div>
   );
